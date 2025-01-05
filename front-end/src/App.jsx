0657125132cdeb8ec85b2ae72c4be46602c982a4
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-01-05 17:16:24 +0100

    add right import

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { DefaultLayout } from './components/DefaultLayout'
+import { DefaultLayout } from './components/DefaultLayout/DefaultLayout'
 import { Home } from './pages/Home'
 function App() {
   
