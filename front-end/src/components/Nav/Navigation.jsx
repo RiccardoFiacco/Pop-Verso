a5e320d954e24b56411d24c1e7ca6527202fa150
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-01-05 16:22:34 +0100

    add navigation

--- front-end/src/components/Nav/Navigation.jsx
+++ front-end/src/components/Nav/Navigation.jsx
@@ -1,16 +1,16 @@
 import { NavLink } from "react-router"
-
+import style from './Navigation.module.css'
 export function Navigation(){
     return(
         <div className="row">
             <div className="col">
-             <NavLink to="/">Home</NavLink>
+             <NavLink className={`${style.none}`} to="/">Home</NavLink>
             </div>
             <div className="col">
-             <NavLink to="/pgs">Personaggi</NavLink>
+             <NavLink className={`${style.none}`} to="/pgs">Personaggi</NavLink>
             </div>
             <div className="col">
-             <NavLink to="/teams"></NavLink>
+             <NavLink className={`${style.none}`} to="/teams">teams</NavLink>
             </div>
         </div>
     )
