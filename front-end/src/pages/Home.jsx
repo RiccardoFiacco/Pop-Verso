6aa9ff7d5475db5e4527631132f6739c2433fcdb
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-01-05 17:16:01 +0100

    done a homepage

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,7 +1,13 @@
 export function Home(){
     return(
-        <>
-         <p>HOME</p>
-        </>
+        <div className="container">
+            <div className="row pt-5">
+                <div className="col pt-5 text-center">
+                    home page di pop verso!!<br/>
+                    Scopri i gruppi e i personaggi che ne fanno parte
+                </div>
+            </div>
+    
+        </div>
     )
 }
