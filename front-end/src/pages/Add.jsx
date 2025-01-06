2b1b769fbb975cea90e88e91cbab9dad422d781e
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-01-06 14:15:56 +0100

    create add page

---
+++ front-end/src/pages/Add.jsx
@@ -0,0 +1,11 @@
+export function Add() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+            <span>ciao</span>
+        </div>
+      </div>
+    </div>
+  );
+}
