2b36217991446a05693828b2dda78cf04059b9f1
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-01-06 14:15:13 +0100

    change the button style

--- front-end/src/components/Card/Card.jsx
+++ front-end/src/components/Card/Card.jsx
@@ -8,7 +8,7 @@ export function Card({obj = {}}){
          <div className="card-body">
              <h5 className="card-title">{name}</h5>
              <p className="card-text">{description}</p>
-             <Link to={age?`/pgs/${id}`:`/teams/${id}`} className="btn btn-primary">Dettagli</Link>
+             <Link to={age?`/pgs/${id}`:`/teams/${id}`} className="btn btn-warning">Dettagli</Link>
          </div>
         </div>
     )
